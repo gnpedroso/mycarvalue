1f87212ed09acc6adca43ec61877009473c4a112
Gabriel Pedroso <gnp_pedroso@hotmail.com>
2023-04-13 18:22:09 +0100

    creating user routes and services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entitity';
@@ -12,5 +12,27 @@ export class UsersService {
         const user = this.repo.create({ email, password });
 
         return this.repo.save(user);
+    }
+
+    findOne(id: number) {
+        return this.repo.findOneBy({ id });
+    }
+
+    find(email: string) {
+        return this.repo.find({ where: { email } });
+    }
+
+    // Partial<User> means that attrs can have id, email or password, or none of them 
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+        if (!user) throw new NotFoundException('user not found!');
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        if (!user) throw new NotFoundException('user not found!');
+        return this.repo.remove(user);
     }
 }
